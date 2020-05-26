7b1a8fe40374a942e75c12edf2bb2bd3402d1761
ANIKET PATHAK <aanickpathak@gmail.com>
2020-05-26 19:02:48 +0530

    Create programme_5th.cpp

---
+++ programme_5th.cpp
@@ -0,0 +1,91 @@
+// This is a programme to study concept of parameterized constructors
+
+#include<iostream>
+#include<cmath>
+
+using namespace std ;
+
+class Rectangle 
+{
+	private :
+		float length ;
+		float breadth ;
+		
+	public :
+		
+		// setxxx    ----->>>>>>>   is a mutator here
+		// getxxx    ----->>>>>>>   is an accessor here
+		 
+		void setLength( float l ) {
+			if ( l >= 0 ) {
+				length = l ;
+			}
+			else {
+				length = 0 ;
+			}
+		}
+		
+		void setBreadth( float b ) {
+			if ( b >= 0 ) {
+				breadth = b ;
+			}
+			else {
+				breadth = 0 ;
+			}
+		}
+		
+		float getLength() {
+			return length ;
+		}
+		
+		float getBreadth() {
+			return breadth ;
+		}
+		
+		// this is a parameterized constructor
+		Rectangle(int l , int b) {
+			setLength(l) ;
+			setBreadth(b) ;
+		}
+		
+		float area() {
+			return (length*breadth) ;
+		}
+		
+		float perimeter() {
+			return (2*(length + breadth)) ;
+		}
+		
+		float diagonal() {
+			return ( sqrt( (length*length) + (breadth*breadth) ) ) ;
+		}
+		
+};
+
+int main() {
+	Rectangle r(10 , 5) ;
+	
+	cout << "Length of rectangle by constructor = " << r.getLength() << endl ;
+	cout << "Breadth of rectangle by constructor = " << r.getBreadth() << endl ;
+	
+	float L , B ;
+	
+	cout << "\nEnter the length of rectangle = " ;
+	cin >> L ;
+	cout << "Enter the breadth of rectangle = " ;
+	cin >> B ;
+	
+	r.setLength( L ) ;
+	r.setBreadth( B ) ;
+	
+	cout << "\nThe length is = " << r.getLength() << endl ;
+	cout << "The breadth is = " << r.getBreadth() << endl ;
+	
+	cout << "\nThe area of rectangle is = " << r.area() << endl ;
+	cout << "The perimeter of rectangle is = " << r.perimeter() << endl ;
+	cout << "The diagonal of rectangle is = " << r.diagonal() <<endl ;
+	
+	return 0 ;
+ 	
+}
+
