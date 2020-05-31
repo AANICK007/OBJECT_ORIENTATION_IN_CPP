7abd3a0636a863077ec01ad91157c6bdaf4c2fc6
ANIKET PATHAK <aanickpathak@gmail.com>
2020-05-31 08:06:30 +0530

    Create programme_16th.cpp

---
+++ programme_16th.cpp
@@ -0,0 +1,108 @@
+// This is a programme to show inheritence through rectangle and cuboid in OOPS
+#include<iostream>
+#include<cmath>
+
+using namespace std ;
+
+class Rectangle {
+	private :
+		float length ;
+		float breadth ;
+		
+	public :
+		Rectangle ( float l = 0 , float b = 0 ) {
+			length = l ;
+			breadth = b ;
+		}
+		void setLength ( float l ) {
+			length = l ;
+		}
+		void setBreadth ( float b ) {
+			breadth = b ;
+		}
+		float getLength() {
+			return length ;
+		}
+		float getBreadth() {
+			return breadth ;
+		}
+		float area() {
+			return ( length * breadth ) ;
+		}
+		float perimeter() {
+			return ( 2 * ( length + breadth ) ) ;
+		}
+		float diagonal() {
+			return ( sqrt( ( length*length ) + ( breadth*breadth ) ) ) ;
+		}
+};
+
+class Cuboid : public Rectangle {
+	private :
+		float height ;
+	
+	public :
+		Cuboid ( float l = 0 , float b = 0 , float h = 0 ) {
+			height = h ;
+			setLength ( l ) ;
+			setBreadth ( b ) ;
+		}
+		void setHeight ( float h ) {
+			height = h ;
+		}
+		float getHeight() {
+			return height ;
+		}
+		float Diagonal() {
+			return ( sqrt ( ( getLength() * getLength() ) + ( getBreadth() * getBreadth() ) + ( height * height ) ) ) ;
+		}
+		float volume() {
+			return ( getLength() * getBreadth() * height ) ;
+		}
+		float curv_s_area() {
+			return ( 2 * ( ( getLength() * getBreadth() ) + ( getBreadth() * height ) ) ) ;
+		}
+		float tot_s_area() {
+			return ( 2 * ( ( getLength() * getBreadth() ) + ( getBreadth() * height ) + ( getLength() * height ) ) ) ;
+		}	
+};
+
+int main() {
+	Rectangle R ;
+	float L , B ;
+	cout << "Enter the length of rectangle = " ;
+	cin >> L ;
+	cout << "Enter the breadth of rectangle = " ;
+	cin >> B ;
+	R.setLength( L ) ;
+	R.setBreadth( B ) ;
+	cout << "\nTHE PROFILE OF RECTANGLE .................." << endl ;
+	cout << "The length of rectangle is = " << R.getLength() << endl ;
+	cout << "The breadth of rectangle is = " << R.getBreadth() << endl ;
+	cout << "The area of rectangle is = " << R.area() << endl ;
+	cout << "The perimeter of rectangle is = " << R.perimeter() << endl ;
+	cout << "The diagonal of rectangle is = " << R.diagonal() << endl ;
+	
+	cout << "\n\n\n" << endl ;
+	Cuboid C ;
+	float H ;
+	cout << "Enter the length of cuboid = " ;
+	cin >> L ;
+	cout << "Enter the breadth of cuboid = " ;
+	cin >> B ;
+	cout << "Enter the height of cuboid = " ;
+	cin >> H ;
+	C.setLength( L ) ;
+	C.setBreadth( B ) ;
+	C.setHeight( H ) ;
+	cout << "\nTHE PROFILE OF CUBOID .................." << endl ;
+	cout << "The length of cuboid is = " << C.getLength() << endl ;
+	cout << "The breadth of cuboid is = " << C.getBreadth() << endl ;
+	cout << "The height of cuboid is = " << C.getHeight() << endl ;
+	cout << "The diagonal of cuboid is = " << C.Diagonal() << endl ;
+	cout << "The curved surface area of cuboid is = " << C.curv_s_area() << endl ;
+	cout << "The total surface area of cuboid is = " << C.tot_s_area() << endl ;
+	cout << "The volume of cuboid is = " << C.volume() << endl ;
+	
+	return 0 ;
+}
